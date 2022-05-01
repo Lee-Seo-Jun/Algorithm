475cd9f86a29249fc5e12658963fe99186389ac9
ionloi <flying9199@gmail.com>
2022-05-01 11:18:24 +0900

    algorithm

---
+++ getMultiRandomValues.js
@@ -0,0 +1,26 @@
+
+
+function rand(min,max,times, array = []){
+
+    /*
+    min=랜덤 최소값 
+    max=랜덤 최대값 
+    times = 몇 개 구할 것인지
+    array = 입력받지 않음
+    */
+    
+        if(array.length===times){
+            return;
+        };
+    
+        let res = Math.random() * (max-min+1);
+        res = Math.floor(res)+min;
+    
+        array.push(res);
+    
+        rand(min,max,times,array);
+    
+    
+        return array;
+    }
+
